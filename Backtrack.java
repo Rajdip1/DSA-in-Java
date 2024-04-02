3702163171a2742fb5591e8742ea949410e0e604
Rajdipsinh Solanki <rajdipsolanki0000@gmail.com>
2024-04-02 15:23:17 +0530

    Adding files

---
+++ Backtrack.java
@@ -0,0 +1,22 @@
+// time comp = O(n * n!)
+
+public class Backtrack {
+    public static void printPermutation(String str, String perm, int index){
+        if(str.length()==0){
+            System.out.println(perm);
+            return;
+        }
+        for(int i=0; i<str.length(); i++){
+            char currenrChar = str.charAt(i);
+
+            String newString = str.substring(0,i)+str.substring(i+1);
+
+            printPermutation(newString, perm+currenrChar, index+1);
+        }
+    }
+
+    public static void main(String[] args){
+        String str = "ABC";
+        printPermutation(str, "", 0);
+    }
+}
