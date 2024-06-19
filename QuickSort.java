7ac438ed347be27ae1da90778a6d902c4521c1c6
Rajdipsinh Solanki <rajdipsolanki0000@gmail.com>
2024-06-19 10:07:08 +0530

    Adding files

---
+++ QuickSort.java
@@ -0,0 +1,48 @@
+import java.io.*;
+
+public class QuickSort {
+    public static int partition(int arr[], int low, int high){
+        int pivot = arr[high];
+        int i = low-1;  //tracks empty spaces
+
+        for(int j=low; j<high; j++){
+            if(arr[j]<pivot){
+                i++;
+
+                //swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        //swap with pivot
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[high];  //posotion of pivot
+        arr[high] = temp;
+        return i;  //pivot index
+    }
+
+    public static void quickSort(int arr[], int low, int high){
+        if(low < high){
+            int pidx = partition(arr, low, high);
+
+            quickSort(arr, low, pidx-1);    //before pivot
+            quickSort(arr, pidx+1, high);   //after pivot
+        }
+    }
+
+    public static void main(String[] args){
+        int arr[] = {6,3,9,5,2,8};
+        int n = arr.length;
+
+        quickSort(arr, 0, n-1);
+
+        //printing array
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+}
